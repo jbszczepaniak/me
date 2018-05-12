14f25cf79d52e02a2c3a6e45b01d8b07700f935a
Jedrzej <jbszczepaniak@gmail.com>
2018-05-12 23:22:21 +0200

    Customize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 export class Section {
-  title string
-  collapsed boolean
-  items []string
+  title: string;
+  collapsed: boolean;
+  items: Item[];
 }
 export class Item {
-  title string
-  description string
-  url string
+  title: string;
+  description: string;
+  url: string;
 }
 
 @Component({
@@ -23,12 +23,22 @@ export class AppComponent {
 
   public ngOnInit() {
     this.sections = [
-      {title: 'What I am doing?', collapsed: false, items:[{title: "gophercises", description: "Well tested solutions for exercises from http://gophercises.com", url: "https://github.com/jedruniu/gophercises-solutions" }]},
-      {title: 'What I am reading?', collapsed: true},
-      {title: 'What I did?', collapsed: true},
-      {title: 'What I read', collapsed: true},
-      {title: 'What I will do?', collapsed: true},
-      {title: 'What I will read?', collapsed: true},
+      {
+        title: 'What I am doing?',
+        collapsed: false,
+        items: [
+          {
+            title: 'gophercises',
+            description: 'Well tested solutions for exercises from http://gophercises.com',
+            url: 'https://github.com/jedruniu/gophercises-solutions',
+          }
+      ]
+      },
+      {title: 'What I am reading?', collapsed: true, items: []},
+      {title: 'What I did?', collapsed: true, items: []},
+      {title: 'What I read', collapsed: true, items: []},
+      {title: 'What I will do?', collapsed: true, items: []},
+      {title: 'What I will read?', collapsed: true, items: []},
     ];
   }
   public open(section) {
